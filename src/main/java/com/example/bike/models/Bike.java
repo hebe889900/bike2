f95943ae68b5a66f832564148c7c74c45bee0c1d
Yan <jackyan63@gmail.com>
2018-06-06 12:26:43 -0400

    Finnaly repositories work

--- src/main/java/com/example/bike/models/Bike.java
+++ src/main/java/com/example/bike/models/Bike.java
@@ -3,8 +3,18 @@ package com.example.bike.models;
 import java.math.BigDecimal;
 import java.sql.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Entity
 public class Bike {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
 	private String name;
 	private String email;
 	private String phone;
@@ -60,5 +70,11 @@ public class Bike {
 	}
 	public void setContact(boolean contact) {
 		this.contact = contact;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
 	}
 }
