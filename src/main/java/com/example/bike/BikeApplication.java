1d57f6c634566deed60a2bda049cdd1796947822
Yan <jackyan63@gmail.com>
2018-06-07 12:42:03 -0400

    Finally fixed the error

--- src/main/java/com/example/bike/BikeApplication.java
+++ src/main/java/com/example/bike/BikeApplication.java
@@ -3,7 +3,7 @@ package com.example.bike;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.bike")
 public class BikeApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/bike/controllers/BikesController.java
+++ src/main/java/com/example/bike/controllers/BikesController.java
@@ -3,6 +3,7 @@ package com.example.bike.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,10 +14,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.bike.models.Bike;
+import com.example.bike.repositories.BikeRepository;
 
 @RestController
 @RequestMapping("/api/v1/bikes")
 public class BikesController {
+	@Autowired
+	private BikeRepository bikeRepository;
+	
 	@GetMapping
 	public List<Bike> list() {
 		List<Bike> bikes = new ArrayList<>();
